59e5e026c039ce1f4244c32aef5c7d4a5d4d2e73
Samuel Perez <samuelp@socialnative.com>
2017-06-09 18:35:35 -0700

    Add ig service, option to login users once

--- index.js
+++ index.js
@@ -1,80 +1,33 @@
-const Client = require('instagram-private-api').V1;
-const storage = new Client.CookieMemoryStorage();
 const co = require('co');
 const bluebird = require('bluebird');
 const argv = require('optimist').argv;
 const {getInstagramUsers, createUserCycler} = require('./backend_service');
-
-const MAX_NUMBER_REQUESTS_PER_USER = 10;
-const MIN_NUMBER_REQUESTS_PER_USER = 1;
-
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
-const getRandomNumberRequests = () => getRandomInt(MIN_NUMBER_REQUESTS_PER_USER, MAX_NUMBER_REQUESTS_PER_USER);
-
-const MAX_RANDOM_WAIT = 10000; // 15 seconds
-const MIN_RANDOM_WAIT = 3000; // 5 seconds
-
-const getRandomWait = () => getRandomInt(MIN_RANDOM_WAIT, MAX_RANDOM_WAIT);
-
-const MAX_RANDOM_LOGIN_WAIT = 3000;
-const MIN_RANDOM_LOGIN_WAIT = 1000;
-
-const getLoginWait = () => getRandomInt(MIN_RANDOM_LOGIN_WAIT, MAX_RANDOM_LOGIN_WAIT);
-
-const crawlnextaccounts = co.wrap(function*() {
-    let accountslefttocrawl = getRandomNumberRequests();
-
-    const {username, password} = getNextUser();
-
-    try {
-        const device = new Client.Device(username);
-        const session = yield Client.Session.create(device, storage, username, password);
-
-        console.log('logged in, waiting to make ' + accountslefttocrawl + ' requests...');
-        yield bluebird.Promise.delay(getLoginWait());
-
-        while(accountslefttocrawl-- > 0) {
-            let nextid = yield redisservice.getnextidtocrawl();
-            if (!nextid) {
-                return null;
-            }
-            console.log('about to crawl: ' + nextid);
-            let useraccount = yield Client.Account.getById(session, nextid);
-            yield redisservice.saveresult(useraccount._params);
-
-            console.log('waiting before next request');
-            yield bluebird.Promise.delay(getRandomWait());
-        }
-    } catch (err) {
-        console.log('something is wrong...');
-        console.log(err);
-    }
-
-    return true;
-});
-
-const parallelism = 2;
-
-const kickoff = co.wrap(function*() {
-    while (true) {
-        try {
-            yield (Array.apply(0, Array(parallelism)).map((val) => crawlnextaccounts()));
-            console.log('finished a set, continuing...');
-        } catch (err) {
-            console.log('error while executing a set');
-            console.log(err);
-            break;
-        }
-    }
-
-    console.log('done!');
-});
+const {createSessionWithUser, kickoff} = require('./instagram_service');
 
 if (argv.getUsers) {
     getInstagramUsers().then((users) => {
         console.log('The following users have been assigned to your machine:');
         console.log(JSON.stringify(users, null, 4));
     });
+} else if (argv.loginUsersOnce) {
+    co.wrap(function*() {
+        const users = yield getInstagramUsers();
+        console.log('Got the following users: ');
+        console.log(users);
+        for (let index in users) {
+            let user = users[index];
+            try {
+                yield createSessionWithUser(user);
+                console.log('Successfully logged in ' + user.username);
+
+            } catch (err) {
+                console.log('Could not log in ' + user.username + '!! Try logging in to IG');
+                console.log(err);
+            }
+            console.log('Waiting before trying next user...');
+            yield bluebird.Promise.delay(1000);
+        }
+    })().then(() => console.log('done'));
 } else {
     kickoff().then(() => console.log('woot'));
 }
---
+++ instagram_service.js
@@ -0,0 +1,74 @@
+const co = require('co');
+const Client = require('instagram-private-api').V1;
+const storage = new Client.CookieMemoryStorage();
+
+const MAX_NUMBER_REQUESTS_PER_USER = 10;
+const MIN_NUMBER_REQUESTS_PER_USER = 1;
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const getRandomNumberRequests = () => getRandomInt(MIN_NUMBER_REQUESTS_PER_USER, MAX_NUMBER_REQUESTS_PER_USER);
+
+const MAX_RANDOM_WAIT = 10000; // 15 seconds
+const MIN_RANDOM_WAIT = 3000; // 5 seconds
+
+const getRandomWait = () => getRandomInt(MIN_RANDOM_WAIT, MAX_RANDOM_WAIT);
+
+const MAX_RANDOM_LOGIN_WAIT = 3000;
+const MIN_RANDOM_LOGIN_WAIT = 1000;
+
+const getLoginWait = () => getRandomInt(MIN_RANDOM_LOGIN_WAIT, MAX_RANDOM_LOGIN_WAIT);
+
+const crawlnextaccounts = co.wrap(function*() {
+    let accountslefttocrawl = getRandomNumberRequests();
+
+    const {username, password} = getNextUser();
+
+    try {
+        const device = new Client.Device(username);
+        const session = yield Client.Session.create(device, storage, username, password);
+
+        console.log('logged in, waiting to make ' + accountslefttocrawl + ' requests...');
+        yield bluebird.Promise.delay(getLoginWait());
+
+        while(accountslefttocrawl-- > 0) {
+            let nextid = yield redisservice.getnextidtocrawl();
+            if (!nextid) {
+                return null;
+            }
+            console.log('about to crawl: ' + nextid);
+            let useraccount = yield Client.Account.getById(session, nextid);
+            yield redisservice.saveresult(useraccount._params);
+
+            console.log('waiting before next request');
+            yield bluebird.Promise.delay(getRandomWait());
+        }
+    } catch (err) {
+        console.log('something is wrong...');
+        console.log(err);
+    }
+
+    return true;
+});
+
+const parallelism = 2;
+
+exports.kickoff = co.wrap(function*() {
+    while (true) {
+        try {
+            yield (Array.apply(0, Array(parallelism)).map((val) => crawlnextaccounts()));
+            console.log('finished a set, continuing...');
+        } catch (err) {
+            console.log('error while executing a set');
+            console.log(err);
+            break;
+        }
+    }
+
+    console.log('done!');
+});
+
+exports.createSessionWithUser = co.wrap(function*(user) {
+    const device = new Client.Device(user.username);
+    const session = yield Client.Session.create(device, storage, user.username, user.password);
+});
+
