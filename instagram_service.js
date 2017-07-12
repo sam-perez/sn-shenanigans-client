5495acc5f649e7eba511877297643ad3d4d8b2ea
Samuel Perez <samuelp@socialnative.com>
2017-07-12 13:57:26 -0700

    Small improvements to run time and error handling

--- instagram_service.js
+++ instagram_service.js
@@ -3,6 +3,7 @@ const Client = require('instagram-private-api').V1;
 const storage = new Client.CookieMemoryStorage();
 const statusLog = require('single-line-log').stdout;
 const bluebird = require('bluebird');
+const argv = require('optimist').argv;
 
 const MAX_NUMBER_REQUESTS_PER_USER = 10;
 const MIN_NUMBER_REQUESTS_PER_USER = 1;
@@ -10,13 +11,13 @@ const MIN_NUMBER_REQUESTS_PER_USER = 1;
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
 const getRandomNumberRequests = () => getRandomInt(MIN_NUMBER_REQUESTS_PER_USER, MAX_NUMBER_REQUESTS_PER_USER);
 
-const MAX_RANDOM_WAIT = 10000;
-const MIN_RANDOM_WAIT = 3000;
+const MAX_RANDOM_WAIT = argv.requestWaitMax || 5000;
+const MIN_RANDOM_WAIT = argv.requestWaitMin || 3000;
 
 const getRandomWait = () => getRandomInt(MIN_RANDOM_WAIT, MAX_RANDOM_WAIT);
 
-const MAX_RANDOM_LOGIN_WAIT = 3000;
-const MIN_RANDOM_LOGIN_WAIT = 1000;
+const MAX_RANDOM_LOGIN_WAIT = argv.loginWaitMax || 3000;
+const MIN_RANDOM_LOGIN_WAIT = argv.loginWaitMin || 1000;
 
 const getLoginWait = () => getRandomInt(MIN_RANDOM_LOGIN_WAIT, MAX_RANDOM_LOGIN_WAIT);
 
@@ -49,18 +50,23 @@ const crawlNextAccounts = co.wrap(function*(getNextUser, getNextIdToCrawl, saveR
             yield bluebird.Promise.delay(getRandomWait());
         }
     } catch (err) {
-        console.log('something is wrong...');
-        console.log(err);
-        user.reportAsBad();
+        if (err.name === 'NotFoundError') {
+            // this is fine, just skip
+            console.log('Page was not found, skipping...');
+        } else {
+            console.log('something is wrong...');
+            console.log(err);
+            user.reportAsBad();
+        }
     }
 
     console.log(`${user.username} is done making requests`);
     return true;
 });
 
-const PARALLELISM = 2;
-const EXECUTIONS_BETWEEN_LONG_SLEEP = 50;
-const LONG_SLEEP_DURATION_IN_SECONDS = 60 * 30; // 30 min long sleep
+const PARALLELISM = argv.parallelism || 2;
+const EXECUTIONS_BETWEEN_LONG_SLEEP = argv.loopsBetweenSleep || 50;
+const LONG_SLEEP_DURATION_IN_SECONDS = argv.sleepDuration || 60 * 30; // 30 min long sleep
 
 exports.kickoff = co.wrap(function*(getInstagramUsers, createUserCycler, saveResult, getNextIdToCrawl) {
     const users = yield getInstagramUsers();
@@ -69,26 +75,31 @@ exports.kickoff = co.wrap(function*(getInstagramUsers, createUserCycler, saveRes
     while (true) {
         const getNextUser = createUserCycler(users);
 
-        let loopsLeftUntilLongSleep = EXECUTIONS_BETWEEN_LONG_SLEEP;
-        while (loopsLeftUntilLongSleep > 0) {
-            try {
-                const results = yield (Array.apply(0, Array(PARALLELISM)).map(
-                    (val) => crawlNextAccounts(getNextUser, getNextIdToCrawl, saveResult)
-                ));
-
-                const shouldStop = results.some((result) => result === false);
-                if (shouldStop) {
-                    break;
-                }
-                console.log('finished a set, continuing...');
-            } catch (err) {
-                console.log('error while executing a set');
-                console.log(err);
-                break;
+        const loopsLeftUntilLongSleep = Array.apply(0, Array(EXECUTIONS_BETWEEN_LONG_SLEEP));
+        let shouldStop = false;
+        const generateLooper = co.wrap(function*() {
+            while (loopsLeftUntilLongSleep.length > 0 && !shouldStop) {
+                console.log(`${loopsLeftUntilLongSleep.length} loops left until we enter hibernation...`)
+                loopsLeftUntilLongSleep.pop();
+                const success = yield crawlNextAccounts(getNextUser, getNextIdToCrawl, saveResult);
+                shouldStop = shouldStop || !success;
             }
-            loopsLeftUntilLongSleep--;
-            console.log(`${loopsLeftUntilLongSleep} loops left until we enter hibernation...`)
+        });
+
+        try {
+            yield (Array.apply(0, Array(PARALLELISM)).map(
+                (val) => generateLooper()
+            ));
+        } catch (err) {
+            console.log('error while executing a set');
+            console.log(err);
+            break;
         }
+
+        if (shouldStop) {
+            break;
+        }
+
         yield countdownTimer(LONG_SLEEP_DURATION_IN_SECONDS);
     }
 
